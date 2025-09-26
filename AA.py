eccd00b865c2d81f7af5c56ad114ea2bae4fa78f
ROSA <mllr8sa@gmail.com>
2025-09-26 15:29:10 -0300

    AA

--- AA.py
+++
@@ -1,7 +0,0 @@
-import os
-def verificar_arquivos(): 
-    arquivos = os.listdir()
-    for item in arquivos:
-        print(item)
-
-verificar_arquivos()
--- projeto multifuncoes/main.py
+++ projeto multifuncoes/main.py
@@ -1,29 +1,43 @@
 import os, flet as ft
 
 # A ideia será vocês criarem uma interface que faça os comandos exigidos de um sistema simples com as bibliotecas mencionadas acima.
-
-
 # 2 - Criar arquivos dentro da pasta e fora.
-# 4 - Renomear arquivo
-# 5 - Verificar e excluir arquivo
-
-import os
-import flet as ft
 
 def main(page: ft.Page):
-    page.title = "Interface com OS"
+    page.title = "AA"
     page.theme_mode = "dark"
+    page.bgcolor = "#1E1E1E"
+    page.window.width = 550
+    page.window.height = 370
+    page.window.max_width = 550
+    page.window.max_height = 370
+
+# FUNCOES ======================================================
+
+    # 5 - Verificar e excluir arquivo FOI
+    def verificar_excluir(a):
+        texto = texto_principal.value
+        try:
+            if os.path.exists(texto):
+                os.remove(texto)
+                informativo.value = (f"Pasta {texto} deletada com sucesso")
+            else:
+                print("Arquivo não encontrado")
+        except FileExistsError:
+            informativo.value = f"A pasta '{texto}' não existe."
+            
 
     # Função para verificar arquivos FOI
-    def verificar_arquivos(a): 
+    def verificar_arquivos(a):
         arquivos = os.listdir()
-        for item in arquivos:
-            print(item)
+        informativo.value = arquivos
         page.update()
 
-    # Função para criar pastas FOI
+
+    # 1 - Criar uma pasta
+    # 2 - Criar arquivos dentro da pasta e fora.
     def criar_pasta(e):
-        texto = texto_recebido.value
+        texto = texto_principal.value
         try:
             os.mkdir(texto)
             informativo.value = f"Pasta criada: '{texto}'"
@@ -31,9 +45,10 @@ def main(page: ft.Page):
             informativo.value = f"A pasta '{texto}' já existe."
         page.update()
     
+
     # Função para criar arquivos FOI
     def criar_arquivo(e):
-        texto = texto_recebido.value
+        texto = texto_principal.value
         try:
             open(texto, "w").close()
             informativo.value = f"Arquivo criado: '{texto}'"
@@ -41,26 +56,38 @@ def main(page: ft.Page):
             informativo.value = f"Erro: {erro}"
         page.update()
 
+
+    def renomear_arquivos(a):
+        try:
+            texto_principal = ft.TextField(label="Escreva o novo nome do arquivo...")
+            os.rename(texto_principal, texto_principal)
+        except Exception as erro:
+            informativo.value = "Digite os nomes corretamente!"
+        page.update()
+
     # Campos e botões FOI
-    texto_recebido = ft.TextField(label="Escreva o nome do arquivo/pasta...")
-    botao_pasta = ft.ElevatedButton("CRIAR PASTA", bgcolor="PURPLE", color="WHITE", width=200, on_click=criar_pasta)
-    botao_arquivo = ft.ElevatedButton("CRIAR ARQUIVO", bgcolor="CYAN", color="BLACK", width=200, on_click=criar_arquivo)
-    informativo = ft.Text("", size=20, color="white")
-    botao_verificar_arquivos = ft.ElevatedButton("VERIFICAR ARQUIVOS", bgcolor="CYAN", color="BLACK", width=200, on_click=verificar_arquivos)
+    texto1 = ft.Text("PROJETO MULTIFUNÇÕES", size=25, color="white" )
+    texto_principal = ft.TextField(label="Escreva o nome do arquivo/pasta...")
+    botao_pasta = ft.ElevatedButton("CRIAR PASTA", bgcolor="BLACK", color="WHITE", width=200, on_click=criar_pasta)
+    botao_arquivo = ft.ElevatedButton("CRIAR ARQUIVO", bgcolor="BLACK", color="WHITE", width=200, on_click=criar_arquivo)
+    botao_excluir_arquivo = ft.ElevatedButton("EXCLUIR ARQUIVOS", bgcolor="BLACK", color="WHITE", width=200, on_click= verificar_excluir)
+    botao_renomear = ft.ElevatedButton("RENOMEAR ARQUIVOS", bgcolor="BLACK", color="WHITE", width=200, on_click= renomear_arquivos)
+    botao_listar_arquivos = ft.ElevatedButton("LISTAR ARQUIVOS", bgcolor="BLACK", color="WHITE", width=200, on_click= verificar_arquivos)
+    informativo = ft.Text("", size=15, color="white")
 
-    # excluir arquivo
-    def excluir_arquivo():
-        
-
-
+    
     
 
     # Layout
     page.add(
-        ft.Row([texto_recebido], alignment="center"),
-        ft.Row([botao_pasta, botao_arquivo, botao_verificar_arquivos], alignment="center"),
+        ft.Row("", alignment="center"),
+        ft.Row("", alignment="center"),
+        ft.Row([texto1], alignment="center"),
+        ft.Row([texto_principal], alignment="center"),
+        ft.Row([botao_pasta, botao_arquivo, ], alignment="center"),
+        ft.Row([ botao_renomear, botao_listar_arquivos], alignment="center"),
+        ft.Row([botao_excluir_arquivo], alignment="center"),
         ft.Row([informativo], alignment="center")
-     
     )
 
 ft.app(target=main)
